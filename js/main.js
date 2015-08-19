be48c67e6895d56e50852da578a289c88c720b1f
julius <julius@expectlabs.com>
2015-08-19 16:27:22 -0700

    works

--- js/main.js
+++ js/main.js
@@ -5,12 +5,13 @@ var requestES = function(ids){
 
   $.ajax({
     type: 'POST',
-    url: "localhost:9200/couchpotato/_mget", //TODO: ES query url
-    data: {
+    url: "http://localhost:9200/couchpotato/_mget", //TODO: ES query url
+    // url: "http://c3c831e4.ngrok.io/couchpotato/_mget",
+    data: JSON.stringify({
       "docs": ids
-    }
+    })
   }).done(function(data){
-    console.log('This is the result: ', data);
+    console.log('This is the newest result: ', data);
     addResult(data);
   });
 }
@@ -23,19 +24,22 @@ var handleSempreResponse = function(data){
   // var re = new RegExp(/.*Top value(.*)/);
   // var re = new RegExp('(.*)');
   var names = re.exec(data);
-
   names = names[1].split("   ");
+  console.log(names);
   re = new RegExp(/name ([0-9a-z]*)/);
   var ids = [];
   for (var i = 1; i < names.length; i++) {
+
     var matches = re.exec(names[i]);
     // ids.push(matches[1]); 
-    entry = {
-      _id: matches[1]
+    if(matches){
+      console.log(matches)
+      entry = {
+        _id: matches[1]
+      }
+      ids.push(entry);
     }
-    ids.push(entry);
   }
-
   // console.log(names[1])
   //make array of the results
   //each will have a id); remove the ) when processing
@@ -45,11 +49,16 @@ var handleSempreResponse = function(data){
   requestES(ids);
 }
 
-var addResult = function(entry){
+var addResult = function(entries){
   //add the result to the DOM
-  console.log('appending something', entry);
-  var dom = '<p>' + entry + '</p>';
-  $('.resultsTable').append(dom);
+  console.log('appending something', entries);
+  entries.docs.forEach(function(entry){
+    console.log('adding one entry to DOM ', entry); 
+    var img = '<img src="' + entry._source.image.url + '">'
+    var title = '<h3>'+entry._source.title+'</h3>'
+    var dom = '<p>' + img + '<br>' + title + '<br>' + entry + '</p>';
+    $('.resultsTable').append(dom);
+  })
 }
 
 $("form").submit(function(event){
