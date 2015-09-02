e3e362da9d7107abbf340d88448e681881fc0c38
Christian Mata <christian@expectlabs.com>
2015-09-02 11:48:14 -0700

    Work to try and add results

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var ESresults = [];
+var currentQuery = '';
 
 var requestES = function(ids){
   console.log('making request to ES');
@@ -16,57 +17,94 @@ var requestES = function(ids){
   });
 }
 
+// Function takes the result from SEMPRE & formats it to make it useful
 var handleSempreResponse = function(data){
   console.log('handling sempre response');
+  // Figure out what which type of response we got
+  stringMatches = data.match(/\(string/) // ** Hacky way to check if the result has Strings that we want to display
+
   //Take the result & get the ids out
-  data = data.split("\n")
+  data = data.split("\n");
+  console.log('data:', data);
   var re = new RegExp(/.*Top value.*\{(.*)\}.*/);
   // var re = new RegExp(/.*Top value(.*)/);
   // var re = new RegExp('(.*)');
   var names = re.exec(data);
   names = names[1].split("   ");
   console.log(names);
-  re = new RegExp(/name ([0-9a-z]*)/);
-  var ids = [];
-  for (var i = 1; i < names.length; i++) {
 
-    var matches = re.exec(names[i]);
-    // ids.push(matches[1]); 
-    if(matches){
-      console.log(matches)
-      entry = {
-        _id: matches[1]
+  if(stringMatches == null){
+    // Case that we're going to need to make some ES queries to get the rest of the data
+    re = new RegExp(/name ([0-9a-z]*)/);
+    var ids = [];
+    for (var i = 1; i < names.length; i++) {
+
+      var matches = re.exec(names[i]);
+      // ids.push(matches[1]);
+      if(matches){
+        console.log(matches)
+        entry = {
+          _id: matches[1]
+        }
+        ids.push(entry);
       }
-      ids.push(entry);
     }
+    // console.log(names[1])
+    //make array of the results
+    //each will have a id); remove the ) when processing
+    // ids.forEach(function(id){
+    //   requestES(ids);
+    // });
+    requestES(ids);
   }
-  // console.log(names[1])
-  //make array of the results
-  //each will have a id); remove the ) when processing
-  // ids.forEach(function(id){
-  //   requestES(ids);
-  // });
-  requestES(ids);
+  else{
+    // Case that we have strings that we want to render on the screen
+    re = new RegExp(/string ([0-9a-z]*)/); //First change in the code
+    var strings = [];
+    for (var i = 1; i < names.length; i++) {
+
+      var matches = re.exec(names[i]);
+      strings.push(matches[1]); // Push the string into the display array
+    }
+    // Now render the results onto the DOM
+    addTextResult(strings);
+  }
+}
+
+// Function that takes an array of strings and adds them
+// to the DOM and display the result.
+var addTextResult = function(entries){
+  console.log('appending some strings: ', entries);
+  entries.forEach(function(resultString) {
+    console.log('Adding this string: ', resultString);
+    var span = '<h4 class="resultString">'+ resultString +'<h4><legend><br>'
+    $('.resultsTable').append(span);
+  })
 }
 
 var addResult = function(entries){
   //add the result to the DOM
   console.log('appending something', entries);
   entries.docs.forEach(function(entry){
-    console.log('adding one entry to DOM ', entry); 
-    var img = '<img src="' + entry._source.image.url + '">'
+    console.log('adding one entry to DOM ', entry);
+    var img = '<img class="resultImg" src="' + entry._source.image.url + '" height="100" width="80">'
     var title = '<h3>'+entry._source.title+'</h3>'
-    var dom = '<p>' + img + '<br>' + title + '<br>' + entry + '</p>';
+    var dom = '<p>' + img + '<br>' + title + '<br>' + entry + '</p><legend><br>';
     $('.resultsTable').append(dom);
   })
 }
 
+//***** Action listener *****
+
+// Function to make the call to SEMPRE
 $("form").submit(function(event){
   console.log('user submitted the form', $("input:first").val() );
-  var query = $("input:first").val().split(' ').join('+');
+  currentQuery = $("input:first").val();
+  var query = currentQuery.split(' ').join('+');
   console.log('Heres the parsed query', query);
 
   // empty the previous results
+  $('#queryString').empty()
   $('.resultsTable').empty();
 
   // begin the process
@@ -79,6 +117,9 @@ $("form").submit(function(event){
       $("input:first").val('');
     }
   })
+
+  // Add the currentQuery as a title to the DOM
+  $('#queryString').append('<h2><i>query: </i> '+ currentQuery +'</h2><br>');
 
   event.preventDefault();
 });
